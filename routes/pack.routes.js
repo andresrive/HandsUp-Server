7bc2cc48afc5649c03494b082d687d5f63608823
Andres Rivera <andreseriverar@gmail.com>
2023-03-14 18:45:30 +0100

    auth context y mas

--- routes/pack.routes.js
+++ routes/pack.routes.js
@@ -1,70 +1,70 @@
 const express = require("express");
 const router = express.Router();
+const { isAuthenticated } = require("../middleware/jwt.middleware.js");
 
-// ℹ️ Handles password encryption
-const jwt = require("jsonwebtoken");
-
-// Require the User model in order to interact with the database
 const User = require("../models/User.model");
 const Pack = require("../models/Pack.model");
 const Plan = require("../models/Plan.model");
 
-// Require necessary (isAuthenticated) middleware in order to control access to specific routes
-const { isAuthenticated } = require("../middleware/jwt.middleware.js");
-
 
 // GET ALL PACKS
-router.get("/", (req,res,next) => {
+router.get("/", (req, res, next) => {
     Pack.find()
-    .populate("author")
-    .then(response => {
-        res.json(response)
-    })
-    .catch(err => console.log(err))
+        .populate("author")
+        .then(response => {
+            res.json(response)
+        })
+        .catch(err => console.log(err))
 })
 
 //POST CREATED PACK
-router.post("/create", (req,res,next) => {
-    const {title, description, images, date, itinerary, destination, price} = req.body
-    Pack.create({title, description, images, date, itinerary, destination, price})
-    .then(response => {
-        res.json(response)
-    })
-    .catch(err => console.log(err))
+router.post("/create", isAuthenticated, (req, res, next) => {
+    const userId = req.payload._id
+
+    const { title, description, images, fromDate, toDate, itinerary, destination, price } = req.body
+    Pack.create({ title, description, images, fromDate, toDate, itinerary, destination, price })
+        .then(response => {
+            User.findByIdAndUpdate(userId, { $push: { packsMade: response } })
+                .then((response) => {
+                    res.json({ result: "ok" })
+                })
+                .catch(err => next(err))
+        })
+        .catch(err => next(err))
 })
 
 //GET ONE PACK
-router.get("/:packId", (req,res,next) => {
-    const {packId} = req.params
+router.get("/:packId", (req, res, next) => {
+    const { packId } = req.params
     Pack.findById(packId)
-    .then(response => {
-        return res.json(response)
+        .then(response => {
+            return res.json(response)
 
-    })
-    .catch(err => console.log(err))
+        })
+        .catch(err => console.log(err))
 })
 
 //PUT EDIT PACK
-router.put("/:packId/edit", (req,res,next) => {
+router.put("/:packId/edit", (req, res, next) => {
     const { packId } = req.params;
     const { title, description, images, date, itinerary, destination, price } = req.body;
 
-    Pack.findByIdAndUpdate(packId, { title, description, images, date, itinerary, destination, price }, {new: true})
-    .then(result => {
-        res.json(result);
-    })
-    .catch(err => next(err))
-    
+    Pack.findByIdAndUpdate(packId, { title, description, images, date, itinerary, destination, price }, { new: true })
+        .then(result => {
+            res.json(result);
+        })
+        .catch(err => next(err))
+
 })
 
 //DELETE PACK
-router.delete("/:packId/delete", (req,res,next) => {
-    const {packId} = req.params;
+router.delete("/:packId/delete", (req, res, next) => {
+    const { packId } = req.params;
     Pack.findByIdAndDelete(packId)
-    .then(response => {
-        res.json({resultado: "ok"});
-    })
-    .catch(err => next(err))
+        .then(response => {
+            res.json({ resultado: "ok" });
+        })
+        .catch(err => next(err))
 })
 
 module.exports = router;
