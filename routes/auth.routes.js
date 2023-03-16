51474fca9b846351fd462eea646c9e49f240627b
Andres Rivera <andreseriverar@gmail.com>
2023-03-16 17:51:58 +0100

    jueves tarde

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -64,8 +64,8 @@ router.post("/login", (req, res, next) => {
       const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
 
       if (passwordCorrect) {
-        const { _id, username, email, images, plansMade, isAdmin, isCompany } = foundUser;
-        const payload = { _id, username, email, images, plansMade, isAdmin, isCompany };
+        const { _id, username, email, images, plansMade, isAdmin, isCompany, plansEnrolled } = foundUser;
+        const payload = { _id, username, email, images, plansMade, isAdmin, isCompany, plansEnrolled };
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
           algorithm: "HS256",
           expiresIn: "6h",
